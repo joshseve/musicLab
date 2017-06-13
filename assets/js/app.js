0aa6471e40a481c5f699c9ac6f9475774d769726
joshseve <josh.seve@gmail.com>
2017-06-13 16:56:20 -0500

    obteniendo objeto usuario

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,3 +20,13 @@ function checkLoginState() {
     statusChangeCallback(response);
   });
 }
+
+FB.getLoginStatus(function(response) {
+  if (response.status === 'connected') {
+    console.log(response.authResponse.accessToken);
+  }
+});
+
+FB.api('/me', function(response) {
+    console.log(JSON.stringify(response));
+});
